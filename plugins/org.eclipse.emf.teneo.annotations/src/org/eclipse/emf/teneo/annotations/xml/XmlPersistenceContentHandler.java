ab9feda0bda9a0f1f2d75dbd6f2dc16447b1c7a3
mtaal <mtaal>
2006-09-05 17:22:24 +0000

    Reorganised imports

--- plugins/org.eclipse.emf.teneo.annotations/src/org/eclipse/emf/teneo/annotations/xml/XmlPersistenceContentHandler.java
+++ plugins/org.eclipse.emf.teneo.annotations/src/org/eclipse/emf/teneo/annotations/xml/XmlPersistenceContentHandler.java
@@ -12,7 +12,6 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.emf.ecore.EcoreFactory;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.teneo.annotations.pamodel.PAnnotatedEClass;
 import org.eclipse.emf.teneo.annotations.pamodel.PAnnotatedEPackage;
