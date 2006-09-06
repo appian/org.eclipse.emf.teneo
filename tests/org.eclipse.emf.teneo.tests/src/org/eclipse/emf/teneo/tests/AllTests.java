8a30df68771025e3ba0261e142dd2e5c41146804
mtaal <mtaal>
2006-09-06 11:36:15 +0000

    Enabled hb tests

--- tests/org.eclipse.emf.teneo.tests/src/org/eclipse/emf/teneo/tests/AllTests.java
+++ tests/org.eclipse.emf.teneo.tests/src/org/eclipse/emf/teneo/tests/AllTests.java
@@ -11,7 +11,7 @@
  *   Martin Taal
  * </copyright>
  *
- * $Id: AllTests.java,v 1.7 2006/09/03 19:31:48 mtaal Exp $
+ * $Id: AllTests.java,v 1.8 2006/09/06 11:36:15 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.tests;
@@ -23,36 +23,14 @@ import junit.framework.TestSuite;
  * Runs all tests.
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.7 $
+ * @version $Revision: 1.8 $
  */
 public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("All tests for hibernate");
-		suite.addTest(org.eclipse.emf.teneo.hibernate.test.emf.annotations.AllTests.suite());
-		suite.addTest(org.eclipse.emf.teneo.hibernate.test.emf.detach.AllTests.suite());
-		suite.addTest(org.eclipse.emf.teneo.hibernate.test.emf.elist.AllTests.suite());
-		suite.addTest(org.eclipse.emf.teneo.hibernate.test.emf.relation.AllTests.suite());
-		suite.addTest(org.eclipse.emf.teneo.hibernate.test.emf.sample.AllTests.suite());
-		suite.addTest(org.eclipse.emf.teneo.hibernate.test.emf.schemaconstructs.AllTests.suite());
-		suite.addTest(org.eclipse.emf.teneo.hibernate.test.issues.AllTests.suite());
-
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.annotations.AllGenerateTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.detach.AllGenerateTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.elist.AllGenerateTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.relation.AllGenerateTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.sample.AllGenerateTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.schemaconstructs.AllGenerateTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.issues.AllGenerateTests.suite());
-//
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.annotations.AllTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.detach.AllTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.elist.AllTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.relation.AllTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.sample.AllTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.emf.schemaconstructs.AllTests.suite());
-//		suite.addTest(org.eclipse.emf.teneo.jpox.test.issues.AllTests.suite());
-		
+		suite.addTest(org.eclipse.emf.teneo.jpox.test.AllTests.suite());
+		suite.addTest(org.eclipse.emf.teneo.hibernate.test.AllTests.suite());
 		return suite;
 	}
 
