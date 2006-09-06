f8858858a15da7a7549a4089bd0dff5b50c4d1f3
mtaal <mtaal>
2006-09-06 10:58:32 +0000

    Added copyright statement

--- plugins/org.eclipse.emf.teneo.annotations/src/org/eclipse/emf/teneo/annotations/xml/XmlPersistenceContentHandler.java
+++ plugins/org.eclipse.emf.teneo.annotations/src/org/eclipse/emf/teneo/annotations/xml/XmlPersistenceContentHandler.java
@@ -1,3 +1,20 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2005, 2006 Springsite BV (The Netherlands) and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Laurens Fridael
+ *   Martin Taal
+ * </copyright>
+ *
+ * $Id: XmlPersistenceContentHandler.java,v 1.4 2006/09/06 10:58:32 mtaal Exp $
+ */
+
 package org.eclipse.emf.teneo.annotations.xml;
 
 import java.util.List;
