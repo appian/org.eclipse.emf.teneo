6b7ba9826d89fe66a9d6a2af033e23ebf6333618
mtaal <mtaal>
2006-11-12 00:08:32 +0000

    [163634] [164035] [164242]

--- plugins/org.eclipse.emf.teneo.hibernate/src/org/eclipse/emf/teneo/hibernate/mapping/ENumUserIntegerType.java
+++ plugins/org.eclipse.emf.teneo.hibernate/src/org/eclipse/emf/teneo/hibernate/mapping/ENumUserIntegerType.java
@@ -1,19 +1,3 @@
-/**
- * <copyright>
- *
- * Copyright (c) 2005, 2006 Springsite BV (The Netherlands) and others
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Martin Taal
- * </copyright>
- *
- * $Id: ENumUserIntegerType.java,v 1.3 2006/11/01 16:19:45 mtaal Exp $
- */
-
 package org.eclipse.emf.teneo.hibernate.mapping;
 
 import java.sql.PreparedStatement;
@@ -32,14 +16,14 @@ import org.eclipse.emf.teneo.hibernate.mapper.HbMapperConstants;
 import org.hibernate.HibernateException;
 
 /**
- * Implements the EMF UserType for an Enum where the value is stored as an int.
+ * Stores a 
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.3 $ $Date: 2006/11/01 16:19:45 $
+ * @version $Revision: 1.4 $ $Date: 2006/11/12 00:08:32 $
  */
 
 public class ENumUserIntegerType extends ENumUserType {
-
+	
 	/** The sql types used for enums */
 	private static final int[] SQL_TYPES = new int[] { Types.INTEGER };
 
