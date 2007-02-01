501aee3283eb41a0cc8870ab5b92030fecf38c86
mtaal <mtaal>
2007-02-01 12:34:26 +0000

    Updated year in copyright/license statement

--- plugins/org.eclipse.emf.teneo.jpox.eclipse/src/org/eclipse/emf/teneo/jpox/eclipse/ImportXML.java
+++ plugins/org.eclipse.emf.teneo.jpox.eclipse/src/org/eclipse/emf/teneo/jpox/eclipse/ImportXML.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2005, 2006 Springsite BV (The Netherlands) and others
+ * Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,7 +11,7 @@
  *   Martin Taal
  * </copyright>
  *
- * $Id: ImportXML.java,v 1.2 2006/09/13 10:39:54 mtaal Exp $
+ * $Id: ImportXML.java,v 1.3 2007/02/01 12:34:26 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.jpox.eclipse;
@@ -32,7 +32,7 @@ import org.eclipse.emf.teneo.jpox.JpoxUtil;
  * Performs the import xml action
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 
 public class ImportXML extends StoreImportXML {
