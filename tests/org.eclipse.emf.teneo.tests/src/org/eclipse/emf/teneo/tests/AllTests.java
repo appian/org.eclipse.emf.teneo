1b4cff5f9093023f2e6fbacdc5192c331efe830d
mtaal <mtaal>
2007-02-01 12:34:07 +0000

    Updated year in copyright/license statement

--- tests/org.eclipse.emf.teneo.tests/src/org/eclipse/emf/teneo/tests/AllTests.java
+++ tests/org.eclipse.emf.teneo.tests/src/org/eclipse/emf/teneo/tests/AllTests.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2005, 2006 Springsite BV (The Netherlands) and others
+ * Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,7 +11,7 @@
  *   Martin Taal
  * </copyright>
  *
- * $Id: AllTests.java,v 1.11 2006/09/08 21:18:43 mtaal Exp $
+ * $Id: AllTests.java,v 1.12 2007/02/01 12:34:07 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.tests;
@@ -23,7 +23,7 @@ import junit.framework.TestSuite;
  * Runs all tests.
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.11 $
+ * @version $Revision: 1.12 $
  */
 public class AllTests {
 
