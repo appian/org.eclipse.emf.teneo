50bf70b267952e96ad7a39fa70ecde9f7ec1e241
mtaal <mtaal>
2007-02-11 21:56:13 +0000

    See update in 0_7_maintenance release

--- doc/org.eclipse.emf.teneo.doc/tutorials/hibernate/samples/QuickStart.java
+++ doc/org.eclipse.emf.teneo.doc/tutorials/hibernate/samples/QuickStart.java
@@ -11,7 +11,7 @@
  *   Martin Taal
  * </copyright>
  *
- * $Id: QuickStart.java,v 1.11 2007/02/05 20:12:02 mtaal Exp $
+ * $Id: QuickStart.java,v 1.12 2007/02/11 21:56:14 mtaal Exp $
  */
 
 package hbtutorial;
@@ -33,7 +33,7 @@ import org.hibernate.Transaction;
  * Quick Start Tutorial
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.11 $
+ * @version $Revision: 1.12 $
 */
 public class QuickStart {
 
--- doc/org.eclipse.emf.teneo.doc/tutorials/jpox/tutorial1/Tutorial1.java
+++ doc/org.eclipse.emf.teneo.doc/tutorials/jpox/tutorial1/Tutorial1.java
@@ -11,7 +11,7 @@
  *   Martin Taal
  * </copyright>
  *
- * $Id: Tutorial1.java,v 1.11 2007/02/05 20:12:02 mtaal Exp $
+ * $Id: Tutorial1.java,v 1.12 2007/02/11 21:56:14 mtaal Exp $
  */
 
 package jpoxtutorial;
@@ -50,7 +50,7 @@ import org.jpox.PMFConfiguration;
  * Tutorial
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.11 $
+ * @version $Revision: 1.12 $
  */
 public class Tutorial1 {
 
