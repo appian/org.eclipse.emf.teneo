56a81696c03833b5a0108da4c743249b7735f168
mtaal <mtaal>
2007-04-17 15:49:36 +0000

    Various changes, removed warnings, solved stringent EListWrapper checking

--- plugins/org.eclipse.emf.teneo.hibernate.eclipse/src/org/eclipse/emf/teneo/hibernate/eclipse/GenerateHBMActionSuperClass.java
+++ plugins/org.eclipse.emf.teneo.hibernate.eclipse/src/org/eclipse/emf/teneo/hibernate/eclipse/GenerateHBMActionSuperClass.java
@@ -1,17 +1,9 @@
 /**
- * <copyright>
- *
- * Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Martin Taal
- * </copyright>
- *
- * $Id: GenerateHBMActionSuperClass.java,v 1.2 2007/02/01 12:34:24 mtaal Exp $
+ * <copyright> Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others All rights
+ * reserved. This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: Martin Taal </copyright> $Id:
+ * GenerateHBMActionSuperClass.java,v 1.2 2007/02/01 12:34:24 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.hibernate.eclipse;
@@ -30,16 +22,19 @@ import org.eclipse.emf.teneo.hibernate.mapper.GenerateHBM;
  * Eclipse popup action to generate a hbm file based on the ecore files.
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 
 public class GenerateHBMActionSuperClass extends GenerateMappingAction {
 	/**
 	 * @see IActionDelegate#run(IAction)
 	 */
+	@Override
 	public void run(IAction action) {
-		final HashMap options = new HashMap();
-		options.put(PersistenceOptions.INHERITANCE_MAPPING, InheritanceType.SINGLE_TABLE_LITERAL.getName());
-		super.run(action, HbConstants.HBM_FILE_NAME, "Generate JDO 2.0 Metadata File", options, GenerateHBM.class.getName());
+		final HashMap<String, String> options = new HashMap<String, String>();
+		options.put(PersistenceOptions.INHERITANCE_MAPPING, InheritanceType.SINGLE_TABLE_LITERAL
+				.getName());
+		super.run(action, HbConstants.HBM_FILE_NAME, "Generate JDO 2.0 Metadata File", options,
+				GenerateHBM.class.getName());
 	}
 }
--- plugins/org.eclipse.emf.teneo.hibernate.eclipse/src/org/eclipse/emf/teneo/hibernate/eclipse/ImportXML.java
+++ plugins/org.eclipse.emf.teneo.hibernate.eclipse/src/org/eclipse/emf/teneo/hibernate/eclipse/ImportXML.java
@@ -1,17 +1,9 @@
 /**
- * <copyright>
- *
- * Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Martin Taal
- * </copyright>
- *
- * $Id: ImportXML.java,v 1.3 2007/02/01 12:34:24 mtaal Exp $
+ * <copyright> Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others All rights
+ * reserved. This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: Martin Taal </copyright> $Id:
+ * ImportXML.java,v 1.3 2007/02/01 12:34:24 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.hibernate.eclipse;
@@ -20,7 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import org.eclipse.core.internal.resources.File;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.teneo.eclipse.StoreEclipseException;
 import org.eclipse.emf.teneo.eclipse.resourcehandler.StoreImportXML;
@@ -32,16 +24,16 @@ import org.eclipse.emf.teneo.hibernate.HbUtil;
  * Performs the import xml action
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 
 public class ImportXML extends StoreImportXML {
 	/** Imports the files into the store */
-	protected void doImport(File[] files, Properties props) {
+	protected void doImport(IFile[] files, Properties props) {
 		try {
 			final HbDataStore emfds = HbUtil.getCreateDataStore(props);
 			for (int i = 0; i < files.length; i++) {
-				final InputStream is = ((File) files[i]).getContents();
+				final InputStream is = files[i].getContents();
 				emfds.importDataStore(is, HbConstants.EXCHANGE_FORMAT_XML);
 				is.close();
 			}
