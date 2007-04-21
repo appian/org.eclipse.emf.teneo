f52301899daeb128aead3c3108d30e4d5c205536
mtaal <mtaal>
2007-04-21 09:22:11 +0000

    [183497] [183495] [181492] [181472] [182989]

--- plugins/org.eclipse.emf.teneo.hibernate/src/org/eclipse/emf/teneo/hibernate/mapping/identifier/IdentifierCacheHandler.java
+++ plugins/org.eclipse.emf.teneo.hibernate/src/org/eclipse/emf/teneo/hibernate/mapping/identifier/IdentifierCacheHandler.java
@@ -1,17 +1,9 @@
 /**
- * <copyright>
- *
- * Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Martin Taal
- * </copyright>
- *
- * $Id: IdentifierCacheHandler.java,v 1.5 2007/02/08 23:11:37 mtaal Exp $
+ * <copyright> Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others All rights
+ * reserved. This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: Martin Taal </copyright> $Id:
+ * IdentifierCacheHandler.java,v 1.5 2007/02/08 23:11:37 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.hibernate.mapping.identifier;
@@ -30,11 +22,11 @@ import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.XMLResource;
 
 /**
- * Implements two maps for caching identifier and version information.
- * Internally uses weakreferences and periodic purge actions to clean the maps.
+ * Implements two maps for caching identifier and version information. Internally uses
+ * weakreferences and periodic purge actions to clean the maps.
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 
 public class IdentifierCacheHandler {
@@ -45,8 +37,7 @@ public class IdentifierCacheHandler {
 	private static final int PURGE_COUNT = 10000;
 
 	/** HashMap */
-	private static Map<Key, Object> idMap = Collections
-			.synchronizedMap(new HashMap<Key, Object>());
+	private static Map<Key, Object> idMap = Collections.synchronizedMap(new HashMap<Key, Object>());
 
 	/** Keeps track of the modifications to the versionMap */
 	private static int idModCount = 0;
@@ -62,17 +53,20 @@ public class IdentifierCacheHandler {
 	public static Object getID(Object obj) {
 		final Object id = idMap.get(new Key(obj));
 		if (id == null) {
-			log.debug("ID for object " + obj.getClass().getName()
-					+ " not found in id cache");
+			log.debug("ID for object " + obj.getClass().getName() + " not found in id cache");
 		}
 		return id;
 	}
 
 	/** Set an identifier in the cache */
 	public static void setID(Object obj, Object id) {
-		log.debug("Setting id: " + id + " for object "
-				+ obj.getClass().getName() + " in idcache ");
-		idMap.put(new Key(obj), id);
+		log.debug("Setting id: " + id + " for object " + obj.getClass().getName() + " in idcache ");
+
+		if (id == null) { // actually a remove of the id
+			idMap.remove(new Key(obj));
+		} else {
+			idMap.put(new Key(obj), id);
+		}
 
 		// also set the id in the resource
 		// disabled for now
@@ -99,8 +93,8 @@ public class IdentifierCacheHandler {
 
 	/** Sets a version in the cache */
 	public static void setVersion(Object obj, Object version) {
-		log.debug("Setting version: " + version + " for object "
-				+ obj.getClass().getName() + " in idcache ");
+		log.debug("Setting version: " + version + " for object " + obj.getClass().getName()
+				+ " in idcache ");
 		versionMap.put(new Key(obj), version);
 		versionModCount++;
 		if (versionModCount > PURGE_COUNT) {
@@ -110,16 +104,14 @@ public class IdentifierCacheHandler {
 
 	/** Purge the versionmap for stale entries */
 	private static synchronized void purgeIDMap() {
-		if (idModCount < PURGE_COUNT)
-			return;
+		if (idModCount < PURGE_COUNT) return;
 		purgeMap(idMap);
 		idModCount = 0;
 	}
 
 	/** Purge the versionmap for stale entries */
 	private static synchronized void purgeVersionMap() {
-		if (versionModCount < PURGE_COUNT)
-			return;
+		if (versionModCount < PURGE_COUNT) return;
 		purgeMap(versionMap);
 		versionModCount = 0;
 	}
@@ -130,9 +122,8 @@ public class IdentifierCacheHandler {
 			final ArrayList<Object> toRemove = new ArrayList<Object>();
 			final Iterator<Key> it = map.keySet().iterator();
 			while (it.hasNext()) {
-				final Key key = (Key) it.next();
-				if (!key.isValid())
-					toRemove.add(key);
+				final Key key = it.next();
+				if (!key.isValid()) toRemove.add(key);
 			}
 			for (int i = 0; i < toRemove.size(); i++) {
 				map.remove(toRemove.get(i));
@@ -150,7 +141,7 @@ public class IdentifierCacheHandler {
 		synchronized (map) {
 			Iterator<Key> it = idMap.keySet().iterator();
 			while (it.hasNext()) {
-				Key key = (Key) it.next();
+				Key key = it.next();
 				Object object = key.weakRef.get();
 				if (object != null) {
 					System.err.println(object.getClass().getName());
@@ -160,8 +151,8 @@ public class IdentifierCacheHandler {
 	}
 
 	/**
-	 * Own implementation of the key in the hashmap to override the equals
-	 * method. Equality for this cache is real memory location equality
+	 * Own implementation of the key in the hashmap to override the equals method. Equality for this
+	 * cache is real memory location equality
 	 */
 
 	private static class Key {
@@ -182,6 +173,7 @@ public class IdentifierCacheHandler {
 		 * 
 		 * @see java.lang.Object#equals(java.lang.Object)
 		 */
+		@Override
 		public boolean equals(Object arg0) {
 			assert (arg0 != null);
 			assert (arg0 instanceof Key);
@@ -213,6 +205,7 @@ public class IdentifierCacheHandler {
 		}
 
 		/** The hashcode of the enclosed object is returned */
+		@Override
 		public int hashCode() {
 			return hashcode;
 		}
