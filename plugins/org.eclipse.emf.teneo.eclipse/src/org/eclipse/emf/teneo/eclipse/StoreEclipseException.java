9709d0b61d1f0c500396841024e20c51f24a76e6
mtaal <mtaal>
2007-06-29 07:30:57 +0000

    Main changes: - Defaultannotator and xml and annotation parsing has moved to the org.eclipse.emf.teneo plugin - SQL Naming has been re-implemented in a separate strategy class - Added option for idbag id column naming

--- plugins/org.eclipse.emf.teneo.eclipse/src/org/eclipse/emf/teneo/eclipse/StoreEclipseException.java
+++ plugins/org.eclipse.emf.teneo.eclipse/src/org/eclipse/emf/teneo/eclipse/StoreEclipseException.java
@@ -1,64 +1,42 @@
 /**
- * <copyright>
- *
- * Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Martin Taal
- * </copyright>
- *
- * $Id: StoreEclipseException.java,v 1.2 2007/02/01 12:35:18 mtaal Exp $
+ * <copyright> Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others All rights
+ * reserved. This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: Martin Taal </copyright> $Id:
+ * StoreEclipseException.java,v 1.2 2007/02/01 12:35:18 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.eclipse;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.eclipse.emf.teneo.TeneoException;
 
 /**
- * Is used to throw runtime plugin exception.
- * This class offers automatic logging to commons logging.
- * Note that this class extends RuntimeException, so no forced throws and catch statements.
- * Although there are very differing views on this topic but it is our experience that 
- * to many checked exceptions only distract the programmer and have no added value.
+ * Is used to throw runtime plugin exception. This class offers automatic logging to commons
+ * logging. Note that this class extends RuntimeException, so no forced throws and catch statements.
+ * Although there are very differing views on this topic but it is our experience that to many
+ * checked exceptions only distract the programmer and have no added value.
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.2 $
-*/
+ * @version $Revision: 1.3 $
+ */
 
-public class StoreEclipseException extends RuntimeException 
-{	
+public class StoreEclipseException extends TeneoException {
 	/**
 	 * Serializable id
 	 */
 	private static final long serialVersionUID = 7433341056815136427L;
-	
-	/** The logger for all these exceptions */
-	private static Log log = LogFactory.getLog(StoreEclipseException.class);
-	
+
 	/**
 	 * The constructor, logs the exception also
 	 */
-	public StoreEclipseException(String msg, Throwable cause)
-	{
+	public StoreEclipseException(String msg, Throwable cause) {
 		super(msg, cause);
-		
-		// and log it
-		log.error(msg, cause);
 	}
 
 	/**
 	 * The constructor, logs the exception also
 	 */
-	public StoreEclipseException(String msg)
-	{
+	public StoreEclipseException(String msg) {
 		super(msg);
-		
-		// and log it
-		log.error(msg, this);
 	}
 }
