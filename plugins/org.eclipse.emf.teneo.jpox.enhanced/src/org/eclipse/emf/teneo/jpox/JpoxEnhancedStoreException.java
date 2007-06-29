e4e35a4c9a96d421a58f62078276125e9966fb29
mtaal <mtaal>
2007-06-29 07:32:07 +0000

    Main changes: - Defaultannotator and xml and annotation parsing has moved to the org.eclipse.emf.teneo plugin - SQL Naming has been re-implemented in a separate strategy class - Added option for idbag id column naming

--- plugins/org.eclipse.emf.teneo.jpox.enhanced/src/org/eclipse/emf/teneo/jpox/JpoxEnhancedStoreException.java
+++ plugins/org.eclipse.emf.teneo.jpox.enhanced/src/org/eclipse/emf/teneo/jpox/JpoxEnhancedStoreException.java
@@ -1,51 +1,36 @@
 /**
- * <copyright>
- *
- * Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
- *   Martin Taal
- * </copyright>
- *
- * $Id: JpoxEnhancedStoreException.java,v 1.2 2007/02/01 12:35:16 mtaal Exp $
+ * <copyright> Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others All rights
+ * reserved. This program and the accompanying materials are made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html Contributors: Martin Taal </copyright> $Id:
+ * JpoxEnhancedStoreException.java,v 1.2 2007/02/01 12:35:16 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.jpox;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.eclipse.emf.teneo.TeneoException;
 
 /**
- * Is used to throw runtime emf/jpox integration exceptions. This class offers automatic logging to commons logging.
- * Note that this class extends RuntimeException, so no forced throws and catch statements. Although there are very
- * differing views on this topic but it is our experience that to many checked exceptions only distract the programmer
- * and have no added value.
+ * Is used to throw runtime emf/jpox integration exceptions. This class offers automatic logging to
+ * commons logging. Note that this class extends RuntimeException, so no forced throws and catch
+ * statements. Although there are very differing views on this topic but it is our experience that
+ * to many checked exceptions only distract the programmer and have no added value.
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 
-public class JpoxEnhancedStoreException extends RuntimeException {
+public class JpoxEnhancedStoreException extends TeneoException {
 	/**
 	 * Serializable id
 	 */
 	private static final long serialVersionUID = 7433341056815136417L;
-
-	/** The logger for all these exceptions */
-	private static Log log = LogFactory.getLog(JpoxEnhancedStoreException.class);
 
 	/**
 	 * The constructor, logs the exception also
 	 */
 	public JpoxEnhancedStoreException(String msg, Throwable cause) {
 		super(msg, cause);
-
-		// and log it
-		log.error(msg, cause);
 	}
 
 	/**
@@ -53,8 +38,5 @@ public class JpoxEnhancedStoreException extends RuntimeException {
 	 */
 	public JpoxEnhancedStoreException(String msg) {
 		super(msg);
-
-		// and log it
-		log.error(msg, this);
 	}
 }
