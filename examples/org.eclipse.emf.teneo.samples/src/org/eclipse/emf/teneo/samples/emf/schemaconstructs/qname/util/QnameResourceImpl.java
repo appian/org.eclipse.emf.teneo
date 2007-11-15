09baf788dec085523afd4c4b711055832e55c752
mtaal <mtaal>
2007-11-15 19:56:40 +0000

    [198124] [206215]

---
+++ examples/org.eclipse.emf.teneo.samples/src/org/eclipse/emf/teneo/samples/emf/schemaconstructs/qname/util/QnameResourceImpl.java
@@ -0,0 +1,32 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: QnameResourceImpl.java,v 1.1 2007/11/15 19:56:40 mtaal Exp $
+ */
+package org.eclipse.emf.teneo.samples.emf.schemaconstructs.qname.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.emf.teneo.samples.emf.schemaconstructs.qname.util.QnameResourceFactoryImpl
+ * @generated
+ */
+public class QnameResourceImpl extends XMLResourceImpl {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated
+	 */
+	public QnameResourceImpl(URI uri) {
+		super(uri);
+	}
+
+} //QnameResourceImpl
