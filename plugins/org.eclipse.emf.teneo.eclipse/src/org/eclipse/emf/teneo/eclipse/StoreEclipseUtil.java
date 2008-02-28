9b691eef487135c594a2e3787b88ef21139c66bb
mtaal <mtaal>
2008-02-28 07:07:29 +0000

    Added 2008 in copyright statement Added TeneoInternalEObject interface to prevent cglib exception

--- plugins/org.eclipse.emf.teneo.eclipse/src/org/eclipse/emf/teneo/eclipse/StoreEclipseUtil.java
+++ plugins/org.eclipse.emf.teneo.eclipse/src/org/eclipse/emf/teneo/eclipse/StoreEclipseUtil.java
@@ -1,5 +1,5 @@
 /**
- * <copyright> Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others All rights
+ * <copyright> Copyright (c) 2005, 2006, 2007, 2008 Springsite BV (The Netherlands) and others All rights
  * reserved. This program and the accompanying materials are made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html Contributors: Martin Taal </copyright> $Id:
@@ -26,7 +26,7 @@ import org.eclipse.ui.PlatformUI;
  * Convenience util, for example reads the property file and returns the property file.
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class StoreEclipseUtil {
 	/** Utility method to handle an error */
