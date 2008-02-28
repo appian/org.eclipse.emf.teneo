ba78fbd5a1b70cb1049d870bdc2d50f4391b9546
mtaal <mtaal>
2008-02-28 07:08:07 +0000

    Added 2008 in copyright statement Added TeneoInternalEObject interface to prevent cglib exception

--- examples/org.eclipse.emf.teneo.samples/src/org/eclipse/emf/teneo/samples/emf/annotations/joincolumns/PersonID.java
+++ examples/org.eclipse.emf.teneo.samples/src/org/eclipse/emf/teneo/samples/emf/annotations/joincolumns/PersonID.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  *
- * Copyright (c) 2005, 2006, 2007 Springsite BV (The Netherlands) and others
+ * Copyright (c) 2005, 2006, 2007, 2008 Springsite BV (The Netherlands) and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,7 +11,7 @@
  *   Martin Taal
  * </copyright>
  *
- * $Id: PersonID.java,v 1.3 2007/02/01 12:35:23 mtaal Exp $
+ * $Id: PersonID.java,v 1.4 2008/02/28 07:08:07 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.samples.emf.annotations.joincolumns;
@@ -23,7 +23,7 @@ import java.io.Serializable;
  * The id class used in the composite id test case
  *  
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
 */
 public class PersonID implements Serializable {
 	
