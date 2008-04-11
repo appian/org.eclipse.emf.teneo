18d1a1eb17887808d06d35936492f7935a157a44
mtaal <mtaal>
2008-04-11 03:21:47 +0000

    Added testcase for large map value

---
+++ examples/org.eclipse.emf.teneo.samples/src/org/eclipse/emf/teneo/samples/issues/largemapvalue/TestElement.java
@@ -0,0 +1,74 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: TestElement.java,v 1.1 2008/04/11 03:21:47 mtaal Exp $
+ */
+package org.eclipse.emf.teneo.samples.issues.largemapvalue;
+
+import org.eclipse.emf.common.util.EMap;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Test Element</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.eclipse.emf.teneo.samples.issues.largemapvalue.TestElement#getTestMap <em>Test Map</em>}</li>
+ *   <li>{@link org.eclipse.emf.teneo.samples.issues.largemapvalue.TestElement#getTestProp <em>Test Prop</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.eclipse.emf.teneo.samples.issues.largemapvalue.LargemapvaluePackage#getTestElement()
+ * @model
+ * @generated
+ */
+public interface TestElement extends EObject {
+	/**
+	 * Returns the value of the '<em><b>Test Map</b></em>' map.
+	 * The key is of type {@link java.lang.String},
+	 * and the value is of type {@link java.lang.String},
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Test Map</em>' map isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Test Map</em>' map.
+	 * @see org.eclipse.emf.teneo.samples.issues.largemapvalue.LargemapvaluePackage#getTestElement_TestMap()
+	 * @model mapType="org.eclipse.emf.teneo.samples.issues.largemapvalue.StringToStringMap<org.eclipse.emf.ecore.EString, org.eclipse.emf.ecore.EString>"
+	 * @generated
+	 */
+	EMap<String, String> getTestMap();
+
+	/**
+	 * Returns the value of the '<em><b>Test Prop</b></em>' attribute.
+	 * <!-- begin-user-doc -->
+	 * <p>
+	 * If the meaning of the '<em>Test Prop</em>' attribute isn't clear,
+	 * there really should be more of a description here...
+	 * </p>
+	 * <!-- end-user-doc -->
+	 * @return the value of the '<em>Test Prop</em>' attribute.
+	 * @see #setTestProp(String)
+	 * @see org.eclipse.emf.teneo.samples.issues.largemapvalue.LargemapvaluePackage#getTestElement_TestProp()
+	 * @model annotation="teneo.jpa value='@Lob\n@Column(length=1000000)'"
+	 * @generated
+	 */
+	String getTestProp();
+
+	/**
+	 * Sets the value of the '{@link org.eclipse.emf.teneo.samples.issues.largemapvalue.TestElement#getTestProp <em>Test Prop</em>}' attribute.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param value the new value of the '<em>Test Prop</em>' attribute.
+	 * @see #getTestProp()
+	 * @generated
+	 */
+	void setTestProp(String value);
+
+} // TestElement
---
+++ examples/org.eclipse.emf.teneo.samples/src/org/eclipse/emf/teneo/samples/issues/largemapvalue/util/LargemapvalueAdapterFactory.java
@@ -0,0 +1,144 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: LargemapvalueAdapterFactory.java,v 1.1 2008/04/11 03:21:47 mtaal Exp $
+ */
+package org.eclipse.emf.teneo.samples.issues.largemapvalue.util;
+
+import java.util.Map;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notifier;
+
+import org.eclipse.emf.common.notify.impl.AdapterFactoryImpl;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.eclipse.emf.teneo.samples.issues.largemapvalue.*;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Adapter Factory</b> for the model.
+ * It provides an adapter <code>createXXX</code> method for each class of the model.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.emf.teneo.samples.issues.largemapvalue.LargemapvaluePackage
+ * @generated
+ */
+public class LargemapvalueAdapterFactory extends AdapterFactoryImpl {
+	/**
+	 * The cached model package.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected static LargemapvaluePackage modelPackage;
+
+	/**
+	 * Creates an instance of the adapter factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public LargemapvalueAdapterFactory() {
+		if (modelPackage == null) {
+			modelPackage = LargemapvaluePackage.eINSTANCE;
+		}
+	}
+
+	/**
+	 * Returns whether this factory is applicable for the type of the object.
+	 * <!-- begin-user-doc -->
+	 * This implementation returns <code>true</code> if the object is either the model's package or is an instance object of the model.
+	 * <!-- end-user-doc -->
+	 * @return whether this factory is applicable for the type of the object.
+	 * @generated
+	 */
+	@Override
+	public boolean isFactoryForType(Object object) {
+		if (object == modelPackage) {
+			return true;
+		}
+		if (object instanceof EObject) {
+			return ((EObject)object).eClass().getEPackage() == modelPackage;
+		}
+		return false;
+	}
+
+	/**
+	 * The switch that delegates to the <code>createXXX</code> methods.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected LargemapvalueSwitch<Adapter> modelSwitch =
+		new LargemapvalueSwitch<Adapter>() {
+			@Override
+			public Adapter caseTestElement(TestElement object) {
+				return createTestElementAdapter();
+			}
+			@Override
+			public Adapter caseStringToStringMap(Map.Entry<String, String> object) {
+				return createStringToStringMapAdapter();
+			}
+			@Override
+			public Adapter defaultCase(EObject object) {
+				return createEObjectAdapter();
+			}
+		};
+
+	/**
+	 * Creates an adapter for the <code>target</code>.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param target the object to adapt.
+	 * @return the adapter for the <code>target</code>.
+	 * @generated
+	 */
+	@Override
+	public Adapter createAdapter(Notifier target) {
+		return modelSwitch.doSwitch((EObject)target);
+	}
+
+
+	/**
+	 * Creates a new adapter for an object of class '{@link org.eclipse.emf.teneo.samples.issues.largemapvalue.TestElement <em>Test Element</em>}'.
+	 * <!-- begin-user-doc -->
+	 * This default implementation returns null so that we can easily ignore cases;
+	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * @return the new adapter.
+	 * @see org.eclipse.emf.teneo.samples.issues.largemapvalue.TestElement
+	 * @generated
+	 */
+	public Adapter createTestElementAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for an object of class '{@link java.util.Map.Entry <em>String To String Map</em>}'.
+	 * <!-- begin-user-doc -->
+	 * This default implementation returns null so that we can easily ignore cases;
+	 * it's useful to ignore a case when inheritance will catch all the cases anyway.
+	 * <!-- end-user-doc -->
+	 * @return the new adapter.
+	 * @see java.util.Map.Entry
+	 * @generated
+	 */
+	public Adapter createStringToStringMapAdapter() {
+		return null;
+	}
+
+	/**
+	 * Creates a new adapter for the default case.
+	 * <!-- begin-user-doc -->
+	 * This default implementation returns null.
+	 * <!-- end-user-doc -->
+	 * @return the new adapter.
+	 * @generated
+	 */
+	public Adapter createEObjectAdapter() {
+		return null;
+	}
+
+} //LargemapvalueAdapterFactory
