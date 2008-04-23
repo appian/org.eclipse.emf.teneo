eadec5e48744ade8a78f2601b72daa889d38806e
mtaal <mtaal>
2008-04-23 15:45:25 +0000

    [227449] [227911] [227673]

--- plugins/org.eclipse.emf.teneo.hibernate/src/org/eclipse/emf/teneo/hibernate/mapping/property/EListPropertyHandler.java
+++ plugins/org.eclipse.emf.teneo.hibernate/src/org/eclipse/emf/teneo/hibernate/mapping/property/EListPropertyHandler.java
@@ -24,7 +24,7 @@ import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.InternalEObject;
 import org.eclipse.emf.ecore.InternalEObject.EStore;
 import org.eclipse.emf.ecore.impl.BasicEObjectImpl;
-import org.eclipse.emf.ecore.impl.EStoreEObjectImpl.BasicEStoreEList;
+import org.eclipse.emf.ecore.impl.EStoreEObjectImpl.EStoreEList;
 import org.eclipse.emf.ecore.util.BasicFeatureMap;
 import org.eclipse.emf.ecore.util.EcoreEMap;
 import org.eclipse.emf.teneo.extension.ExtensionManager;
@@ -56,7 +56,7 @@ import org.hibernate.property.Setter;
  * interfaces. When the getGetter and getSetter methods are called it returns itself.
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.24 $
+ * @version $Revision: 1.25 $
  */
 @SuppressWarnings("unchecked")
 public class EListPropertyHandler implements Getter, Setter, PropertyAccessor, ExtensionPoint, ExtensionManagerAware {
@@ -124,7 +124,7 @@ public class EListPropertyHandler implements Getter, Setter, PropertyAccessor, E
 	public Object get(Object owner) throws HibernateException {
 		Object obj = ((EObject) owner).eGet(eFeature);
 
-		if (obj instanceof BasicEStoreEList<?>) {
+		if (isEStoreList(obj)) {
 			final EStore eStore = ((InternalEObject) owner).eStore();
 			// the call to size forces a load, this is a trick to
 			// force the estore to create a list, otherwise the .get
@@ -174,6 +174,21 @@ public class EListPropertyHandler implements Getter, Setter, PropertyAccessor, E
 		return obj;
 	}
 
+	protected boolean isEStoreList(Object o) {
+		if (o == null) {
+			return false;
+		}
+		if (o instanceof EStoreEList<?>) {
+			return true;
+		}
+		// this really ugly check is to make this code backwards compatible with emf 2.3
+		// the BasicEStoreEList is introduced in emf 2.3
+		if (o.getClass().getName().compareTo("org.eclipse.emf.ecore.impl.EStoreEObjectImpl.BasicEStoreEList") == 0) {
+			return true;
+		}
+		return false;
+	}
+
 	protected String getFieldName(Object owner) {
 		return eFeature.getName();
 	}
@@ -187,7 +202,7 @@ public class EListPropertyHandler implements Getter, Setter, PropertyAccessor, E
 	public Object getForInsert(Object owner, Map mergeMap, SessionImplementor session) throws HibernateException {
 		Object obj = ((EObject) owner).eGet(eFeature);
 
-		if (obj instanceof BasicEStoreEList<?>) {
+		if (isEStoreList(obj)) {
 			final EStore eStore = ((InternalEObject) owner).eStore();
 			// the call to size forces a load, this is a trick to
 			// force the estore to create a list, otherwise the .get
@@ -256,7 +271,7 @@ public class EListPropertyHandler implements Getter, Setter, PropertyAccessor, E
 			}
 			Object currentValue = EcoreAccess.getManyEFeatureValue(eFeature, (BasicEObjectImpl) target);
 
-			if (currentValue instanceof BasicEStoreEList<?>) {
+			if (isEStoreList(currentValue)) {
 				final EStore eStore = ((InternalEObject) target).eStore();
 				if (eStore.size((InternalEObject) target, eFeature) != -1) {
 					currentValue = eStore.get((InternalEObject) target, eFeature, EStore.NO_INDEX);
