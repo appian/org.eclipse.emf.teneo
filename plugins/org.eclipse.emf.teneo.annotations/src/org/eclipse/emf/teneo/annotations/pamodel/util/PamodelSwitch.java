66f73882f9b8f23ee8f06c8a172ca3d6242580b0
mtaal <mtaal>
2008-06-02 07:15:42 +0000

    [234530] [235028]

--- plugins/org.eclipse.emf.teneo.annotations/src/org/eclipse/emf/teneo/annotations/pamodel/util/PamodelSwitch.java
+++ plugins/org.eclipse.emf.teneo.annotations/src/org/eclipse/emf/teneo/annotations/pamodel/util/PamodelSwitch.java
@@ -2,7 +2,7 @@
  * <copyright>
  * </copyright>
  *
- * $Id: PamodelSwitch.java,v 1.24 2008/01/29 12:58:06 mtaal Exp $
+ * $Id: PamodelSwitch.java,v 1.25 2008/06/02 07:15:42 mtaal Exp $
  */
 package org.eclipse.emf.teneo.annotations.pamodel.util;
 
@@ -10,7 +10,6 @@ import java.util.List;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.teneo.annotations.pamodel.*;
 import org.eclipse.emf.teneo.annotations.pamodel.PAnnotatedEAttribute;
 import org.eclipse.emf.teneo.annotations.pamodel.PAnnotatedEClass;
 import org.eclipse.emf.teneo.annotations.pamodel.PAnnotatedEDataType;
@@ -23,31 +22,26 @@ import org.eclipse.emf.teneo.annotations.pamodel.PAnnotatedModel;
 import org.eclipse.emf.teneo.annotations.pamodel.PamodelPackage;
 
 /**
- * <!-- begin-user-doc -->
- * The <b>Switch</b> for the model's inheritance hierarchy.
- * It supports the call {@link #doSwitch(EObject) doSwitch(object)}
- * to invoke the <code>caseXXX</code> method for each class of the model,
- * starting with the actual class of the object
- * and proceeding up the inheritance hierarchy
- * until a non-null result is returned,
- * which is the result of the switch.
+ * <!-- begin-user-doc --> The <b>Switch</b> for the model's inheritance hierarchy. It supports the
+ * call {@link #doSwitch(EObject) doSwitch(object)} to invoke the <code>caseXXX</code> method for
+ * each class of the model, starting with the actual class of the object and proceeding up the
+ * inheritance hierarchy until a non-null result is returned, which is the result of the switch.
  * <!-- end-user-doc -->
+ * 
  * @see org.eclipse.emf.teneo.annotations.pamodel.PamodelPackage
  * @generated
  */
 public class PamodelSwitch<T> {
 	/**
-	 * The cached model package
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * The cached model package <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	protected static PamodelPackage modelPackage;
 
 	/**
-	 * Creates an instance of the switch.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Creates an instance of the switch. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @generated
 	 */
 	public PamodelSwitch() {
@@ -57,9 +51,9 @@ public class PamodelSwitch<T> {
 	}
 
 	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result;
+	 * it yields that result. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
@@ -68,9 +62,9 @@ public class PamodelSwitch<T> {
 	}
 
 	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result;
+	 * it yields that result. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
@@ -78,101 +72,139 @@ public class PamodelSwitch<T> {
 		if (theEClass.eContainer() == modelPackage) {
 			return doSwitch(theEClass.getClassifierID(), theEObject);
 		}
-		else {
-			List<EClass> eSuperTypes = theEClass.getESuperTypes();
-			return
-				eSuperTypes.isEmpty() ?
-					defaultCase(theEObject) :
-					doSwitch(eSuperTypes.get(0), theEObject);
-		}
+		List<EClass> eSuperTypes = theEClass.getESuperTypes();
+		return eSuperTypes.isEmpty() ? defaultCase(theEObject) : doSwitch(eSuperTypes.get(0), theEObject);
 	}
 
 	/**
-	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
-	 * <!-- begin-user-doc -->
-	 * <!-- end-user-doc -->
+	 * Calls <code>caseXXX</code> for each class of the model until one returns a non null result;
+	 * it yields that result. <!-- begin-user-doc --> <!-- end-user-doc -->
+	 * 
 	 * @return the first non-null result returned by a <code>caseXXX</code> call.
 	 * @generated
 	 */
 	protected T doSwitch(int classifierID, EObject theEObject) {
 		switch (classifierID) {
 			case PamodelPackage.PANNOTATED_EMODEL_ELEMENT: {
-				PAnnotatedEModelElement pAnnotatedEModelElement = (PAnnotatedEModelElement)theEObject;
+				PAnnotatedEModelElement pAnnotatedEModelElement = (PAnnotatedEModelElement) theEObject;
 				T result = casePAnnotatedEModelElement(pAnnotatedEModelElement);
-				if (result == null) result = defaultCase(theEObject);
+				if (result == null) {
+					result = defaultCase(theEObject);
+				}
 				return result;
 			}
 			case PamodelPackage.PANNOTATED_MODEL: {
-				PAnnotatedModel pAnnotatedModel = (PAnnotatedModel)theEObject;
+				PAnnotatedModel pAnnotatedModel = (PAnnotatedModel) theEObject;
 				T result = casePAnnotatedModel(pAnnotatedModel);
-				if (result == null) result = defaultCase(theEObject);
+				if (result == null) {
+					result = defaultCase(theEObject);
+				}
 				return result;
 			}
 			case PamodelPackage.PANNOTATED_EPACKAGE: {
-				PAnnotatedEPackage pAnnotatedEPackage = (PAnnotatedEPackage)theEObject;
+				PAnnotatedEPackage pAnnotatedEPackage = (PAnnotatedEPackage) theEObject;
 				T result = casePAnnotatedEPackage(pAnnotatedEPackage);
-				if (result == null) result = casePAnnotatedEModelElement(pAnnotatedEPackage);
-				if (result == null) result = defaultCase(theEObject);
+				if (result == null) {
+					result = casePAnnotatedEModelElement(pAnnotatedEPackage);
+				}
+				if (result == null) {
+					result = defaultCase(theEObject);
+				}
 				return result;
 			}
 			case PamodelPackage.PANNOTATED_ECLASS: {
-				PAnnotatedEClass pAnnotatedEClass = (PAnnotatedEClass)theEObject;
+				PAnnotatedEClass pAnnotatedEClass = (PAnnotatedEClass) theEObject;
 				T result = casePAnnotatedEClass(pAnnotatedEClass);
-				if (result == null) result = casePAnnotatedEModelElement(pAnnotatedEClass);
-				if (result == null) result = defaultCase(theEObject);
+				if (result == null) {
+					result = casePAnnotatedEModelElement(pAnnotatedEClass);
+				}
+				if (result == null) {
+					result = defaultCase(theEObject);
+				}
 				return result;
 			}
 			case PamodelPackage.PANNOTATED_ESTRUCTURAL_FEATURE: {
-				PAnnotatedEStructuralFeature pAnnotatedEStructuralFeature = (PAnnotatedEStructuralFeature)theEObject;
+				PAnnotatedEStructuralFeature pAnnotatedEStructuralFeature = (PAnnotatedEStructuralFeature) theEObject;
 				T result = casePAnnotatedEStructuralFeature(pAnnotatedEStructuralFeature);
-				if (result == null) result = casePAnnotatedETypedElement(pAnnotatedEStructuralFeature);
-				if (result == null) result = casePAnnotatedEModelElement(pAnnotatedEStructuralFeature);
-				if (result == null) result = defaultCase(theEObject);
+				if (result == null) {
+					result = casePAnnotatedETypedElement(pAnnotatedEStructuralFeature);
+				}
+				if (result == null) {
+					result = casePAnnotatedEModelElement(pAnnotatedEStructuralFeature);
+				}
+				if (result == null) {
+					result = defaultCase(theEObject);
+				}
 				return result;
 			}
 			case PamodelPackage.PANNOTATED_EATTRIBUTE: {
-				PAnnotatedEAttribute pAnnotatedEAttribute = (PAnnotatedEAttribute)theEObject;
+				PAnnotatedEAttribute pAnnotatedEAttribute = (PAnnotatedEAttribute) theEObject;
 				T result = casePAnnotatedEAttribute(pAnnotatedEAttribute);
-				if (result == null) result = casePAnnotatedEStructuralFeature(pAnnotatedEAttribute);
-				if (result == null) result = casePAnnotatedETypedElement(pAnnotatedEAttribute);
-				if (result == null) result = casePAnnotatedEModelElement(pAnnotatedEAttribute);
-				if (result == null) result = defaultCase(theEObject);
+				if (result == null) {
+					result = casePAnnotatedEStructuralFeature(pAnnotatedEAttribute);
+				}
+				if (result == null) {
+					result = casePAnnotatedETypedElement(pAnnotatedEAttribute);
+				}
+				if (result == null) {
+					result = casePAnnotatedEModelElement(pAnnotatedEAttribute);
+				}
+				if (result == null) {
+					result = defaultCase(theEObject);
+				}
 				return result;
 			}
 			case PamodelPackage.PANNOTATED_EREFERENCE: {
-				PAnnotatedEReference pAnnotatedEReference = (PAnnotatedEReference)theEObject;
+				PAnnotatedEReference pAnnotatedEReference = (PAnnotatedEReference) theEObject;
 				T result = casePAnnotatedEReference(pAnnotatedEReference);
-				if (result == null) result = casePAnnotatedEStructuralFeature(pAnnotatedEReference);
-				if (result == null) result = casePAnnotatedETypedElement(pAnnotatedEReference);
-				if (result == null) result = casePAnnotatedEModelElement(pAnnotatedEReference);
-				if (result == null) result = defaultCase(theEObject);
+				if (result == null) {
+					result = casePAnnotatedEStructuralFeature(pAnnotatedEReference);
+				}
+				if (result == null) {
+					result = casePAnnotatedETypedElement(pAnnotatedEReference);
+				}
+				if (result == null) {
+					result = casePAnnotatedEModelElement(pAnnotatedEReference);
+				}
+				if (result == null) {
+					result = defaultCase(theEObject);
+				}
 				return result;
 			}
 			case PamodelPackage.PANNOTATED_ETYPED_ELEMENT: {
-				PAnnotatedETypedElement pAnnotatedETypedElement = (PAnnotatedETypedElement)theEObject;
+				PAnnotatedETypedElement pAnnotatedETypedElement = (PAnnotatedETypedElement) theEObject;
 				T result = casePAnnotatedETypedElement(pAnnotatedETypedElement);
-				if (result == null) result = casePAnnotatedEModelElement(pAnnotatedETypedElement);
-				if (result == null) result = defaultCase(theEObject);
+				if (result == null) {
+					result = casePAnnotatedEModelElement(pAnnotatedETypedElement);
+				}
+				if (result == null) {
+					result = defaultCase(theEObject);
+				}
 				return result;
 			}
 			case PamodelPackage.PANNOTATED_EDATA_TYPE: {
-				PAnnotatedEDataType pAnnotatedEDataType = (PAnnotatedEDataType)theEObject;
+				PAnnotatedEDataType pAnnotatedEDataType = (PAnnotatedEDataType) theEObject;
 				T result = casePAnnotatedEDataType(pAnnotatedEDataType);
-				if (result == null) result = casePAnnotatedEModelElement(pAnnotatedEDataType);
-				if (result == null) result = defaultCase(theEObject);
+				if (result == null) {
+					result = casePAnnotatedEModelElement(pAnnotatedEDataType);
+				}
+				if (result == null) {
+					result = defaultCase(theEObject);
+				}
 				return result;
 			}
-			default: return defaultCase(theEObject);
+			default:
+				return defaultCase(theEObject);
 		}
 	}
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>PAnnotated EModel Element</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * <!-- begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>PAnnotated EModel Element</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
 	 * @generated
@@ -183,11 +215,11 @@ public class PamodelSwitch<T> {
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>PAnnotated Model</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * <!-- begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>PAnnotated Model</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
 	 * @generated
@@ -198,11 +230,11 @@ public class PamodelSwitch<T> {
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>PAnnotated EPackage</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * <!-- begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>PAnnotated EPackage</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
 	 * @generated
@@ -213,11 +245,11 @@ public class PamodelSwitch<T> {
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>PAnnotated EClass</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * <!-- begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>PAnnotated EClass</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
 	 * @generated
@@ -228,11 +260,11 @@ public class PamodelSwitch<T> {
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>PAnnotated EStructural Feature</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * <!-- begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>PAnnotated EStructural Feature</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
 	 * @generated
@@ -243,11 +275,11 @@ public class PamodelSwitch<T> {
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>PAnnotated EAttribute</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * <!-- begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>PAnnotated EAttribute</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
 	 * @generated
@@ -258,11 +290,11 @@ public class PamodelSwitch<T> {
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>PAnnotated EData Type</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * <!-- begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>PAnnotated EData Type</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
 	 * @generated
@@ -273,11 +305,11 @@ public class PamodelSwitch<T> {
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>PAnnotated EReference</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * <!-- begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>PAnnotated EReference</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
 	 * @generated
@@ -288,11 +320,11 @@ public class PamodelSwitch<T> {
 
 	/**
 	 * Returns the result of interpreting the object as an instance of '<em>PAnnotated ETyped Element</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * <!-- begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>PAnnotated ETyped Element</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
 	 * @generated
@@ -302,12 +334,12 @@ public class PamodelSwitch<T> {
 	}
 
 	/**
-	 * Returns the result of interpreting the object as an instance of '<em>EObject</em>'.
-	 * <!-- begin-user-doc -->
-	 * This implementation returns null;
-	 * returning a non-null result will terminate the switch, but this is the last case anyway.
-	 * <!-- end-user-doc -->
-	 * @param object the target of the switch.
+	 * Returns the result of interpreting the object as an instance of '<em>EObject</em>'. <!--
+	 * begin-user-doc --> This implementation returns null; returning a non-null result will
+	 * terminate the switch, but this is the last case anyway. <!-- end-user-doc -->
+	 * 
+	 * @param object
+	 *            the target of the switch.
 	 * @return the result of interpreting the object as an instance of '<em>EObject</em>'.
 	 * @see #doSwitch(org.eclipse.emf.ecore.EObject)
 	 * @generated
@@ -316,4 +348,4 @@ public class PamodelSwitch<T> {
 		return null;
 	}
 
-} //PamodelSwitch
+} // PamodelSwitch
