e4219e266c3226d9e9e9de338cf059464078cba1
mtaal <mtaal>
2008-09-20 21:20:12 +0000

    [247785] [247939]

--- tests/org.eclipse.emf.teneo.jpox.test/src/org/eclipse/emf/teneo/jpox/test/TestAnyModelAction.java
+++ tests/org.eclipse.emf.teneo.jpox.test/src/org/eclipse/emf/teneo/jpox/test/TestAnyModelAction.java
@@ -3,7 +3,7 @@
  * reserved. This program and the accompanying materials are made available under the terms of the
  * Eclipse Public License v1.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html Contributors: Martin Taal </copyright> $Id:
- * LibraryAction.java,v 1.8 2007/03/29 15:00:32 mtaal Exp $
+ * SupInterfacesAction.java,v 1.8 2007/03/29 15:00:32 mtaal Exp $
  */
 
 package org.eclipse.emf.teneo.jpox.test;
@@ -18,7 +18,7 @@ import org.eclipse.emf.teneo.test.stores.TestStore;
  * Test
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public class TestAnyModelAction extends AbstractTestAction {
 	/**
