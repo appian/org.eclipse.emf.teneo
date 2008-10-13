c0a5f5a95b506fd6b0c597eabe4953a916542209
mtaal <mtaal>
2008-10-13 05:35:14 +0000

    [250239]

--- tests/org.eclipse.emf.teneo.hibernate.test/src/org/eclipse/emf/teneo/hibernate/test/emf/annotations/AllTests.java
+++ tests/org.eclipse.emf.teneo.hibernate.test/src/org/eclipse/emf/teneo/hibernate/test/emf/annotations/AllTests.java
@@ -54,7 +54,7 @@ import org.eclipse.emf.teneo.test.emf.annotations.VariousAction;
  * All tests
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.70 $
+ * @version $Revision: 1.71 $
  */
 public class AllTests {
 
@@ -62,6 +62,7 @@ public class AllTests {
 		TestSuite suite =
 				new MultiCfgTestSuite("Test for org.eclipse.emf.teneo.hibernate.test.emf.annotations", HibernateTestbed
 					.instance().getConfigurations());
+		suite.addTestSuite(HbSequenceIdAction.class);
 		suite.addTestSuite(UserTypeAction.class);
 
 		suite.addTestSuite(HibernateAction.class);
---
+++ tests/org.eclipse.emf.teneo.hibernate.test/src/org/eclipse/emf/teneo/hibernate/test/emf/annotations/HbSequenceIdAction.java
@@ -0,0 +1,75 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2005, 2006, 2007, 2008 Springsite BV (The Netherlands) and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Martin Taal
+ * </copyright>
+ *
+ * $Id: HbSequenceIdAction.java,v 1.1 2008/10/13 05:35:14 mtaal Exp $
+ */
+
+package org.eclipse.emf.teneo.hibernate.test.emf.annotations;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Properties;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+import org.eclipse.emf.teneo.hibernate.HbHelper;
+import org.eclipse.emf.teneo.samples.emf.annotations.hb.generator.id.IdPackage;
+import org.eclipse.emf.teneo.test.AbstractTestAction;
+import org.eclipse.emf.teneo.test.stores.TestStore;
+
+/**
+ * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
+ * @version $Revision: 1.1 $
+ */
+public class HbSequenceIdAction extends AbstractTestAction {
+	/** How many test objects are created */
+	private static final int NO_TEST_OBJECTS = 10;
+
+	// this epackage is actually not tested!
+	public HbSequenceIdAction() {
+		super(IdPackage.eINSTANCE);
+	}
+
+	@Override
+	public void doAction(TestStore store) {
+		final ResourceSet resourceSet = new ResourceSetImpl();
+		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("*", new EcoreResourceFactoryImpl());
+		final ArrayList<EPackage> epackages = new ArrayList<EPackage>();
+		try {
+			final Resource res = resourceSet.createResource(URI.createURI("test.ecore"));
+			res.load(this.getClass().getResourceAsStream("sequenceid.ecore"), Collections.EMPTY_MAP);
+
+			final Iterator<EObject> it = res.getAllContents();
+			while (it.hasNext()) {
+				final Object obj = it.next();
+				if (obj instanceof EPackage) {
+					EPackage epack = (EPackage) obj;
+					if (EPackage.Registry.INSTANCE.getEPackage(epack.getNsURI()) == null) {
+						EPackage.Registry.INSTANCE.put(epack.getNsURI(), epack);
+					}
+					epackages.add(epack);
+				}
+			}
+			System.err.println(HbHelper.INSTANCE.generateMapping(epackages.toArray(new EPackage[epackages.size()]),
+				new Properties()));
+		} catch (Exception e) {
+			throw new Error(e);
+		}
+	}
+}
