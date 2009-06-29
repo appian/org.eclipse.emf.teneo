473bb3c59be55469dc06c6f1e63aae5ee150d00f
mtaal <mtaal>
2009-06-29 22:54:08 +0000

    [280355]

--- tests/org.eclipse.emf.teneo.hibernate.test/src/org/eclipse/emf/teneo/hibernate/test/issues/AllTests.java
+++ tests/org.eclipse.emf.teneo.hibernate.test/src/org/eclipse/emf/teneo/hibernate/test/issues/AllTests.java
@@ -46,13 +46,15 @@ import org.eclipse.emf.teneo.test.issues.TopClassesAction;
  * All tests
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.46 $
+ * @version $Revision: 1.47 $
  */
 public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new MultiCfgTestSuite("Test for org.eclipse.emf.teneo.hibernate.test.issues",
 				HibernateTestbed.instance().getConfigurations());
+
+		suite.addTestSuite(Bz280355Action.class);
 
 		suite.addTestSuite(BZ225296_2Action.class);
 		suite.addTestSuite(Bz280480Action.class);
