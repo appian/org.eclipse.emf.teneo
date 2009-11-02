8f831bdad61b53131f90385c7bdc6c9318fc5e6b
mtaal <mtaal>
2009-11-02 19:29:59 +0000

    Disabled test case which results in test case run crash on build server

--- tests/org.eclipse.emf.teneo.hibernate.test/src/org/eclipse/emf/teneo/hibernate/test/issues/AllTests.java
+++ tests/org.eclipse.emf.teneo.hibernate.test/src/org/eclipse/emf/teneo/hibernate/test/issues/AllTests.java
@@ -46,7 +46,7 @@ import org.eclipse.emf.teneo.test.issues.TopClassesAction;
  * All tests
  * 
  * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
- * @version $Revision: 1.57 $
+ * @version $Revision: 1.58 $
  */
 public class AllTests {
 
@@ -102,10 +102,10 @@ public class AllTests {
 		suite.addTestSuite(BZ224991Action.class);
 
 		suite.addTestSuite(AbstractTopClassAction.class);
-		suite.addTestSuite(GeneralTestAction.class);
 		// disabled some more tests as the build server seems to fail here...
 		// these tests pass locally ofcourse
 		if (!HibernateTestbed.isRunningOnBuildServer()) {
+			suite.addTestSuite(GeneralTestAction.class);
 			suite.addTestSuite(TransientOtmAction.class);
 			suite.addTestSuite(UpdateAction.class);
 			suite.addTestSuite(MTMSameAction.class);
