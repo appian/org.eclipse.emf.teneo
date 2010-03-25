36dbd9429d3a5fa7385d8d67bb7331361c7e2937
mtaal <mtaal>
2010-03-25 00:11:46 +0000

    Added new test

---
+++ tests/org.eclipse.emf.teneo.commontest/src/org/eclipse/emf/teneo/test/emf/schemaconstructs/ModelGroupAction.java
@@ -0,0 +1,67 @@
+/**
+ * <copyright>
+ *
+ * Copyright (c) 2005, 2006, 2007, 2008 Springsite BV (The Netherlands) and others
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Martin Taal
+ * </copyright>
+ *
+ * $Id: ModelGroupAction.java,v 1.1 2010/03/25 00:11:46 mtaal Exp $
+ */
+
+package org.eclipse.emf.teneo.test.emf.schemaconstructs;
+
+import java.io.StringWriter;
+import java.math.BigInteger;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.util.FeatureMap;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+import org.eclipse.emf.teneo.Constants;
+import org.eclipse.emf.teneo.samples.emf.schemaconstructs.mixed.LetterBodyType;
+import org.eclipse.emf.teneo.samples.emf.schemaconstructs.mixed.MixedFactory;
+import org.eclipse.emf.teneo.samples.emf.schemaconstructs.mixed.MixedPackage;
+import org.eclipse.emf.teneo.samples.emf.schemaconstructs.mixed.SalutationType;
+import org.eclipse.emf.teneo.samples.emf.schemaconstructs.modelgroup.ModelgroupPackage;
+import org.eclipse.emf.teneo.test.AbstractTestAction;
+import org.eclipse.emf.teneo.test.stores.TestStore;
+
+/**
+ * Tests for the modelgroup construction.
+ * 
+ * @author <a href="mailto:mtaal@elver.org">Martin Taal</a>
+ * @version $Revision: 1.1 $
+ */
+public class ModelGroupAction extends AbstractTestAction {
+	
+	/**
+	 * Constructor for ClassHierarchyParsing.
+	 * 
+	 * @param arg0
+	 */
+	public ModelGroupAction() {
+		super(ModelgroupPackage.eINSTANCE);
+	}
+
+	/** Creates simple types and tests against */
+	@Override
+	public void doAction(TestStore store) {
+		store.disableDrop();
+	}
+}
