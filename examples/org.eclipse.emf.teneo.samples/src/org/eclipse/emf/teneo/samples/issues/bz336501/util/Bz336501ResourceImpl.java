91e3ae211a3ba9fc9368f4df2fa43f9adef22bd2
mtaal <mtaal>
2011-02-21 05:47:40 +0000

    [336501]

---
+++ examples/org.eclipse.emf.teneo.samples/src/org/eclipse/emf/teneo/samples/issues/bz336501/util/Bz336501ResourceImpl.java
@@ -0,0 +1,32 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+ * $Id: Bz336501ResourceImpl.java,v 1.1 2011/02/21 05:47:40 mtaal Exp $
+ */
+package org.eclipse.emf.teneo.samples.issues.bz336501.util;
+
+import org.eclipse.emf.common.util.URI;
+
+import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Resource </b> associated with the package.
+ * <!-- end-user-doc -->
+ * @see org.eclipse.emf.teneo.samples.issues.bz336501.util.Bz336501ResourceFactoryImpl
+ * @generated
+ */
+public class Bz336501ResourceImpl extends XMLResourceImpl {
+	/**
+	 * Creates an instance of the resource.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @param uri the URI of the new resource.
+	 * @generated
+	 */
+	public Bz336501ResourceImpl(URI uri) {
+		super(uri);
+	}
+
+} //Bz336501ResourceImpl
